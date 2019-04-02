fa7b956249c43ba8feae2f6d54888e52e133499c
Aaron Magid <ahm64@case.edu>
2019-04-02 05:55:48 -0400

    test(Reader): Added test for init which replaces stdin for testing.

--- radio/test/test_reader.py
+++ radio/test/test_reader.py
@@ -1,5 +1,22 @@
 import pytest
+import sys
+from io import StringIO
 from radio.reader import Reader
 
-def test_empty():
-    assert True
+class ReplaceStdIn:
+    def __init__(self, input):
+        self._STDIN = sys.stdin
+        sys.stdin = StringIO(input)
+
+    def cleanup(self):
+        sys.stdin = self._STDIN
+
+def test_init():
+    input_backup = ReplaceStdIn("123\n45\n")
+
+    reader = Reader()
+
+    input_backup.cleanup()
+
+    assert reader.recipient_address() == 123
+    assert reader.caller_address() == 45
