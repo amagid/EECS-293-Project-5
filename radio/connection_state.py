83b4a174bbb2d2dd508c2b75f5b15ee8cfedb476
Aaron Magid <ahm64@case.edu>
2019-04-02 02:39:00 -0400

    feat(ConnectionState): Added pseudocode for ConnectionState class into ConnectionState class file.

--- radio/connection_state.py
+++ radio/connection_state.py
@@ -1,2 +1,18 @@
-class ConnectionState():
+# The ConnectionState class represents the current state of the connection.
+# CONNECTED state is static, while FAILURE is a method generating a failed
+# state with the given reason.
+
+class ConnectionState:
     pass
+    # CONNECTED = ""
+    # FAILURE = ""
+    # FAILURE_INVALID_RECIPIENT = " invalid recipient"
+    # FAILURE_RECIPIENT_NOT_ME = " recipient not me"
+    # FAILURE_INVALID_CALLER = " invalid caller"
+
+    # TO_STRING():
+    #     initialize output to empty string
+    #     if self is ConnectionState.CONNECTED, append "true" to output
+    #     else append "false" to output
+    #     append enum instance value to output
+
