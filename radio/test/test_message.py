5f27ddf6cfeaa7ef12c42d44622883f59dfe7894
Aaron Magid <ahm64@case.edu>
2019-04-02 04:45:47 -0400

    test(Message): Finished testing of Message with tests for is_valid() and build().

--- radio/test/test_message.py
+++ radio/test/test_message.py
@@ -9,3 +9,35 @@ def test_init_assigns_values():
 
     assert message.command() is command
     assert message.value() is value
+
+
+BUILD_TEST_CASES = [
+    (Command.TO, 3, False),
+    (Command.TO, None, True),
+    (None, 3, True),
+    (5, 6, True),
+    (Command.INVALID, 4, True),
+    (Command.REP, 7, False)
+]
+@pytest.mark.parametrize(
+    'test_case', BUILD_TEST_CASES
+)
+def test_build(test_case):
+    command = test_case[0]
+    value = test_case[1]
+    expect_message_invalid = test_case[2]
+
+    message = Message.build(command, value)
+
+    assert (message is Message.INVALID) == expect_message_invalid
+
+def test_is_valid_valid():
+    message = Message.build(Command.TO, 3)
+
+    assert message.is_valid()
+
+def test_is_valid_invalid():
+    message = Message.INVALID
+
+    assert not message.is_valid()
+
