c39e36cab12331bfa474af8e8b409930bdadd0ca
Aaron Magid <ahm64@case.edu>
2019-04-02 04:07:47 -0400

    test(Command): Added testing for _command_with_highest_score().

--- radio/test/test_command.py
+++ radio/test/test_command.py
@@ -1,6 +1,17 @@
 import pytest
 from radio.command import Command
 
+# Helper Methods
+
+def _generate_score_dictionary(to_score, rep_score, thisis_score):
+    return {
+        "TO": to_score,
+        "REP": rep_score,
+        "THISIS": thisis_score
+    }
+
+# Tests
+
 LETTERS_IN_COMMON_TEST_CASES = [
     (Command.TO, "TO", 2),
     (Command.REP, "TO", 0),
@@ -18,5 +29,26 @@ def test_letters_in_common(test_case):
     expected = test_case[2]
 
     result = command._letters_in_common(raw_command)
+
+    assert result == expected
+
+
+COMMAND_WITH_HIGHEST_SCORE_TEST_CASES = [
+    (_generate_score_dictionary(0, 0, 0), Command.INVALID),
+    (_generate_score_dictionary(-1, -2, -3), Command.INVALID),
+    (_generate_score_dictionary(1, 0, 0), Command.TO),
+    (_generate_score_dictionary(1, 2, 1), Command.REP),
+    (_generate_score_dictionary(1, 2, 3), Command.THISIS),
+    (_generate_score_dictionary(3, 2, 3), Command.TO)
+]
+@pytest.mark.parametrize(
+    'test_case', COMMAND_WITH_HIGHEST_SCORE_TEST_CASES
+)
+def test_letters_in_common(test_case):
+    command = Command.INVALID
+    score_dictionary = test_case[0]
+    expected = test_case[1]
+
+    result = command._command_with_highest_score(score_dictionary)
 
     assert result == expected
