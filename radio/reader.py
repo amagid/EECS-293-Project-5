2aabbfcb4b4aa89a8e93ff788cc946361f7e2e35
Aaron Magid <ahm64@case.edu>
2019-04-02 05:20:12 -0400

    feat(Reader): Close input stream rather than setting it to None.

--- radio/reader.py
+++ radio/reader.py
@@ -10,17 +10,17 @@ class Reader:
     # Init connects the Reader to standard input
     def __init__(self):
         # Try to read recipient and caller addresses.
-        # If fails for any reason, invalidate the input stream
+        # If fails for any reason, close the input stream
         try:
             self._input = sys.stdin
             self._recipient_address = int(self._input.readline())
             self._caller_address = int(self._input.readline())
         except:
-            self._input = None
+            self._input.close()
 
-        # If addresses were not set for any reason, invalidate the input stream
-        if not isinstance(self.recipient_address(), int) and not isinstance(self.caller_address(), int):
-            self._input = None
+        # If addresses were not set for any reason, close the input stream
+        if not self._input.closed and (not isinstance(self.recipient_address(), int) or not isinstance(self.caller_address(), int)):
+            self._input.close()
 
 
     # Return the actual recipient address (SETUP METHOD)
@@ -49,13 +49,5 @@ class Reader:
     # _NEXT_CHARACTER is an internal method responsible for reading the next character from the input stream
     # Returns None if there is no next character
     def _next_character(self):
-        if self._input_stream_valid():
-            return self._input.read()
-        else:
-            return None
-
-
-    # # _INPUT_STREAM_VALID detecs whether the input stream is still valid
-    # _INPUT_STREAM_VALID():
-    #     return True if input stream is still open and has more characters in it
+        return not self._input.closed and self._input.read(1) or None
 
