590423d9748be7e4283b7d87849f9cd6bacb7dec
Aaron Magid <ahm64@case.edu>
2019-04-02 02:51:52 -0400

    test(ConnectionState): Added parametrized tests for ConnectionState.__str__().

--- radio/test/test_connection_state.py
+++ radio/test/test_connection_state.py
@@ -1,5 +1,18 @@
 import pytest
 from radio.connection_state import ConnectionState
 
-def test_empty():
-    assert True
+TEST_STRINGS = [
+    (ConnectionState.CONNECTED, "true"),
+    (ConnectionState.FAILURE, "false generic error"),
+    (ConnectionState.FAILURE_INVALID_RECIPIENT, "false" + ConnectionState.FAILURE_INVALID_RECIPIENT.value),
+]
+@pytest.mark.parametrize(
+    'test_string_case', TEST_STRINGS
+)
+def test_str(test_string_case):
+    state = test_string_case[0]
+    expected = test_string_case[1]
+
+    result = str(state)
+
+    assert result == expected
