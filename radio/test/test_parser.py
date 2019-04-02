8157126883408a671bc5038c8553c002773e37d7
Aaron Magid <ahm64@case.edu>
2019-04-02 06:49:43 -0400

    test(parser): Tests for init and getters.

--- radio/test/test_parser.py
+++ radio/test/test_parser.py
@@ -1,5 +1,20 @@
 import pytest
+import sys
 from radio.parser import Parser
+from radio.test.utils import ReplaceStdIn
 
-def test_empty():
-    assert True
+def test_init_assigns_properties():
+    input_backup = ReplaceStdIn("123\n45\nTO 1\n")
+    parser = Parser()
+    input_backup.cleanup()
+
+    assert parser._reader.is_valid()
+    assert parser._leftovers is None
+
+def test_address_getters():
+    input_backup = ReplaceStdIn("123\n45\nTO 1\n")
+    parser = Parser()
+    input_backup.cleanup()
+
+    assert parser.recipient_address() == 123
+    assert parser.caller_address() == 45
