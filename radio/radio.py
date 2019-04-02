77abbec62a5d3d4428fc291448f0c7691f184ea9
Aaron Magid <ahm64@case.edu>
2019-04-02 02:37:59 -0400

    feat(Radio): Added pseudocode for Radio class into Radio class file.

--- radio/radio.py
+++ radio/radio.py
@@ -1,2 +1,77 @@
-class Radio():
+# The Radio class is the master class of the program. It initializes an
+# internally stored Parser and uses it to read valid, sanitized Messages from
+# the input stream. It is responsible only for handling high-level protocol
+# errors, and expects that all received Messages will be correctly formatted
+# and exist, or otherwise be marked as INVALID.
+
+class Radio:
     pass
+    # # Controls whether or not exceptions are returned
+    # DEBUG = False
+
+    # # INIT initializes this Radio with an internal Parser
+    # INIT():
+    #     initialize a Parser and store it in self._parser
+    #     initialize self._to_address to None
+    #     initialize self._from_address to None 
+    #     initialize self._command_parsers to a dictionary containing {
+    #         Message.TO = self._PARSE_TO,
+    #         Message.REP = self._PARSE_LONE_REP,
+    #         Message.THISIS = self._PARSE_THISIS,
+    #         Message.INVALID = self._PARSE_INVALID
+    #     }
+
+    
+    # # ATTEMPT_CONNECT is the main method. Reads messages one at a time and parses major message sections based on the received
+    # # message. Reading a message of a particular type kicks off parsing further for that type of message.
+    # # This method also swallows all exceptions when in production to ensure no raw errors ever make it back to the client.
+    # ATTEMPT_CONNECT():
+    #     try:
+    #         store self._parser.NEXT_MESSAGE() in message
+    #         while message is not None and not CONNECTION_VALID():
+    #             read message.Command and run associated parser stored in self._command_parsers
+
+    #         initialize connection_state
+    #         if CONNECTION_VALID():
+    #             store ConnectionState.CONNECTED in connection_state
+    #         else:
+    #             store _FAILED_CONNECTION_STATE() in connection_state
+
+    #         return string representation of connection_state
+    #     catch Exception e:
+    #         if not DEBUG:
+    #             return string representation of generic FAILURE ConnectionState
+    #         else:
+    #             return string representation of received error
+
+
+    # # CONNECTION_VALID is a public method returning whether or not the connection is currently valid
+    # CONNECTION_VALID():
+    #     return True if self._to_address is equal to self._parser.RECIPIENT_ADDRESS() and self._from_address is a valid number equal to self._parser.CALLER_ADDRESS()
+
+
+    # # _FAILED_CONNECTION_STATE returns the proper failure ConnectionState based on the current state information
+    # _FAILED_CONNECTION_STATE():
+    #     initialize error to _CHECK_RECIPIENT_ERROR()
+    #     if error is None
+    #         set error to _CHECK_CALLER_ERROR()
+    #     if error is still None
+    #         set error to ConnectionState.FAILURE
+
+    #     return error
+
+
+    # # _CHECK_RECIPIENT_ERROR returns the appropriate RECIPIENT_ERROR ConnectionState based on the current state information
+    # # Returns None if there is no issue with the recipient information
+    # _CHECK_RECIPIENT_ERROR():
+    #     if self._to_address is invalid or missing, return ConnectionState.FAILURE_INVALID_RECIPIENT
+    #     elif self._to_address is valid but not my address, return ConnectionState.FAILURE_RECIPIENT_NOT_ME
+    #     else return None
+
+
+    # # _CHECK_CALLER_ERROR returns the appropriate CALLER_ERROR ConnectionState based on the current state information
+    # # Returns None if there is no issue with the caller information
+    # _CHECK_CALLER_ERROR():
+    #     if self._from_address is invalid or missing, return ConnectionState.FAILURE_INVALID_CALLER
+    #     else return None
+
