4b7ac61e1ec2a3004b9a36ddf62fb18068dcdba1
Aaron Magid <ahm64@case.edu>
2019-04-02 06:12:56 -0400

    test(Reader): Tests for _next_character.

--- radio/test/test_reader.py
+++ radio/test/test_reader.py
@@ -47,3 +47,27 @@ def test_init_short_stream():
     input_backup.cleanup()
 
     assert not reader.is_valid()
+
+def test_next_character_empty_stream():
+    input_backup = ReplaceStdIn("123\n45\n")
+    reader = Reader()
+    next_char = reader._next_character()
+    input_backup.cleanup()
+
+    assert next_char is None
+
+def test_next_character_single_character_left():
+    input_backup = ReplaceStdIn("123\n45\nT")
+    reader = Reader()
+    next_char = reader._next_character()
+    input_backup.cleanup()
+
+    assert next_char == "T"
+
+def test_next_character_many_characters_left():
+    input_backup = ReplaceStdIn("123\n45\nTO 1\nTO 1\n TO 1")
+    reader = Reader()
+    next_char = reader._next_character()
+    input_backup.cleanup()
+
+    assert next_char == "T"
