8c04f6ef25b9907f74b4f70fb8004b88f93cf0ec
Aaron Magid <ahm64@case.edu>
2019-04-02 04:36:23 -0400

    test(Message): Added tests for message init, command, and value.

--- radio/test/test_message.py
+++ radio/test/test_message.py
@@ -1,5 +1,11 @@
 import pytest
 from radio.message import Message
+from radio.command import Command
 
-def test_empty():
-    assert True
+def test_init_assigns_values():
+    command = Command.TO
+    value = 3
+    message = Message(command, value)
+
+    assert message.command() is command
+    assert message.value() is value
