36859139e9f17529e8096bc5940e9f7f214c624d
Aaron Magid <ahm64@case.edu>
2019-04-02 06:19:30 -0400

    test(Reader): Added tests for next_message().

--- radio/test/test_reader.py
+++ radio/test/test_reader.py
@@ -71,3 +71,47 @@ def test_next_character_many_characters_left():
     input_backup.cleanup()
 
     assert next_char == "T"
+
+def test_next_character_twice_many_characters_left():
+    input_backup = ReplaceStdIn("123\n45\nTO 1\nTO 1\n TO 1")
+    reader = Reader()
+    next_char = reader._next_character()
+    next_char2 = reader._next_character()
+    input_backup.cleanup()
+
+    assert next_char == "T"
+    assert next_char2 == "O"
+
+def test_next_message_many_messages_left():
+    input_backup = ReplaceStdIn("123\n45\nTO 1\nREP 1\n REP 1")
+    reader = Reader()
+    next_message = reader.next_message()
+    input_backup.cleanup()
+
+    assert next_message == "TO 1"
+
+def test_next_message_twice_many_messages_left():
+    input_backup = ReplaceStdIn("123\n45\nTO 1\nREP 1\n REP 1")
+    reader = Reader()
+    next_message = reader.next_message()
+    next_message2 = reader.next_message()
+    input_backup.cleanup()
+
+    assert next_message == "TO 1"
+    assert next_message2 == "REP 1"
+
+def test_next_message_no_messages_left():
+    input_backup = ReplaceStdIn("123\n45\n")
+    reader = Reader()
+    next_message = reader.next_message()
+    input_backup.cleanup()
+
+    assert next_message is None
+
+def test_next_message_empty_message_next():
+    input_backup = ReplaceStdIn("123\n45\n\n\nTO 1\n")
+    reader = Reader()
+    next_message = reader.next_message()
+    input_backup.cleanup()
+
+    assert next_message is None
