71fcc925c6fc5c6ea8ff45ef41035669592a39c1
Aaron Magid <ahm64@case.edu>
2019-04-02 06:38:36 -0400

    feat(parser): First pass at Parser.

--- radio/parser.py
+++ radio/parser.py
@@ -3,45 +3,59 @@
 # Parser is limited to a single message, so it does not verify message order or
 # placement in stream.
 
+from radio.reader import Reader
+
 class Parser:
-    pass
-    # # INIT method initializes a Reader to be stored internally
-    # INIT():
-    #     initialize a Reader and store it in self._reader
-    #     initialize self._leftovers to None
+    # INIT method initializes a Reader to be stored internally
+    def __init__(self):
+        self._reader = Reader()
+        self._leftovers = None
 
     
-    # # Returns my actual recipient address (SETUP METHOD)
-    # RECIPIENT_ADDRESS():
-    #     return self._reader.RECIPIENT_ADDRESS()
+    # Returns my actual recipient address (SETUP METHOD)
+    def recipient_address(self):
+        return self._reader.recipient_address()
 
 
-    # # Returns the actual caller address (DEBUG METHOD)
-    # CALLER_ADDRESS():
-    #     return self._reader.CALLER_ADDRESS()
-
-    
-    # # Returns the next whole message as an instance of the Message class
-    # NEXT_MESSAGE():
-    #     store self._reader.NEXT_MESSAGE in message variable
-    #     store _EXTRACT_COMMAND_AND_VALUE(message) in command, value
-    #     generate Message instance from command and value inputs
-    #     return generated Message instance
+    # Returns the actual caller address (DEBUG METHOD)
+    def caller_address(self):
+        return self._reader.caller_address()
 
     
-    # # Reads a single raw message string and returns the command and value as best it can
-    # _EXTRACT_COMMAND_AND_VALUE(message):
-    #     split message on space delimiter and store in raw_command and raw_value
-    #     store Command.CLOSEST_MATCH(raw_command) in command
-    #     store _CLEAN_VALUE(raw_value) in value, leftovers
-    #     store leftovers in _leftovers
-    #     return command, value
+    # Returns the next whole message as an instance of the Message class
+    def next_message():
+        message = self._reader.next_message()
+        command, value = self._extract_command_and_value(message)
+        return Message(command, value)
 
     
-    # # Parses a potential value, separates and returns the first "good" value it sees as well as the leftovers
-    # _CLEAN_VALUE(raw_value):
-    #     iterate through string until digits found
-    #     store read digits as a number in value
-    #     store remainder of message to right of value in leftovers
-    #     return value, leftovers
+    # Reads a single raw message string and returns the command and value as best it can
+    def _extract_command_and_value(message):
+        message_chunks = message.split(' ')
+        raw_command = message_chunks[0]
+        raw_value = message_chunks[1]
+
+        command = Command.closest_match(raw_command)
+        value, leftovers = self._clean_value(raw_value)
+
+        self._leftovers = leftovers
+        return command, value
+
+    
+    # Parses a potential value, separates and returns the first "good" value it sees as well as the leftovers
+    def _clean_value(raw_value):
+        value_string = ""
+        found_number = False
+        final_index = -1
+        for index in range(len(raw_value)):
+            if character.isdigit():
+                value_string += character
+                found_number = True
+                final_index = index
+            elif found_number:
+                break
+
+        value = int(value_string)
+        leftovers = raw_value[final_index+1:]
+        return value, leftovers
 
